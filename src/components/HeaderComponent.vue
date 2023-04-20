993bf1fe3ddc7f45aaa46dc7fee8b0112234f021
NicolaDiQuinzio <nicola.di.quinzio1@gmail.com>
2023-04-20 14:38:31 +0200

    fix style

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,4 @@
 <template>
-    <header class="pt-3">
         <div class="container">
             <div class="row">
                 <div class="col-6">
@@ -7,13 +6,10 @@
                 </div>
             </div>
         </div>
-
-    </header>
 </template>
 
 <script>
 import { store } from '../store/store';
-import axios from 'axios';
 export default {
     name: 'HeaderComponent',
     data() {
@@ -25,23 +21,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-header {
-    background-color: transparent;
 
     img {
         max-width: 200px;
     }
-
-    button {
-        background-color: #e9292c;
-        color: white;
-        border-radius: 5px;
-        border: 0;
-        padding: .3rem .8rem;
-    }
-
     input {
         border-radius: 5px;
     }
-}
 </style>
